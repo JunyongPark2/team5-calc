04ee016eac73a6ffd6165dbe006f2973544cde8a
JH <jhs0303.kim@samsung.com>
2026-06-30 16:37:55 +0900

    Zegop 기능 구현

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pytest
 
-
 class Calc:
     # 이곳에 코드를 작성
-    pass
+    def getZegop(self, num:int):
+        return num * num
 
 # 테스트 케이스 작성
 def test_sample():
     assert 1 == 1
     pytest.fail()
+
+@pytest.mark.parametrize(("num", "result"), [(1, 1),(2, 4),(3,9),(4,16),(5,25),(6,36),(7,49),(8,64),(9,81),(10,100)])
+def test_zegop(num, result):
+    assert Calc().getZegop(num) == result
