3e9594625f07b8b8d9181d1209e05b3e6c1ef595
junyong <shewco@naver.com>
2026-06-30 16:33:36 +0900

    feat: getSumSum

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pytest
 
 class Calc:
     # 이곳에 코드를 작성
-    pass
+    def getSumSum(a, b, c):
+        return a + b + c
+
 
 # 테스트 케이스 작성
-def test_sample():
-    assert 1 == 1
-    pytest.fail()
+def test_getSumSum():
+    assert Calc.getSumSum(1, 2, 3) == 6
+    assert Calc.getSumSum(0, 0, 0) == 0
+    assert Calc.getSumSum(-1, 2, 1) == 2
